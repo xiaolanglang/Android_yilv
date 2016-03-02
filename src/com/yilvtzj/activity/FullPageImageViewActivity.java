86718a1c597924dbe9f15a0edd74bb98c464b3c4
xiaolanglang <1712877559@qq.com>
2016-03-02 23:10:18 +0800

    Signed-off-by: xiaolanglang <1712877559@qq.com>

--- src/com/yilvtzj/activity/FullPageImageViewActivity.java
+++ src/com/yilvtzj/activity/FullPageImageViewActivity.java
@@ -14,9 +14,14 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.ImageLoader.ImageContainer;
+import com.android.volley.toolbox.ImageLoader.ImageListener;
 import com.yilvtzj.R;
-import com.yilvtzj.util.MyImageLoader;
+import com.yilvtzj.app.LocationApplication;
 import com.yilvtzj.view.ZoomImageView;
+import com.yilvtzj.volley.cache.LruBitmapCache;
 
 /**
  * 全屏查看页面
@@ -29,6 +34,7 @@ public class FullPageImageViewActivity extends Activity {
 	private static String[] mPictures;
 	private static int mSelectId;
 	private static Activity mActivity;
+	private static ImageLoader mImageLoader;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -36,6 +42,7 @@ public class FullPageImageViewActivity extends Activity {
 		setContentView(R.layout.full_page_image);
 		mPictures = getIntent().getExtras().getStringArray("pictures");
 		mSelectId = getIntent().getExtras().getInt("selectId");
+		mImageLoader = new ImageLoader(LocationApplication.requestQueue, new LruBitmapCache());
 
 		ViewPager vp = (ViewPager) findViewById(R.id.viewpager);
 		vp.setOffscreenPageLimit(5);
@@ -55,30 +62,39 @@ public class FullPageImageViewActivity extends Activity {
 		public View instantiateItem(ViewGroup container, final int position) {
 
 			ViewGroup view = (ViewGroup) mActivity.getLayoutInflater().inflate(R.layout.full_page_viewpager, null);
-			ZoomImageView zoomImageView = new ZoomImageView(container.getContext());
+			final ZoomImageView zoomImageView = new ZoomImageView(container.getContext());
 			ProgressBar progress_img = new ProgressBar(mActivity);
 			TextView reload_tv = new TextView(mActivity);
 			reload_tv.setText("加载失败点击重试");
 			reload_tv.setTextColor(mActivity.getResources().getColor(R.color.black));
 
-			RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			RelativeLayout.LayoutParams lp = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT,
+					LayoutParams.WRAP_CONTENT);
 			lp.addRule(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
 			view.addView(zoomImageView, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
 			view.addView(progress_img, lp);
 			view.addView(reload_tv, lp);
 
-			final MyImageLoader myImageLoader = new MyImageLoader();
+			mImageLoader.get(mPictures[position], new ImageListener() {
+
+				@Override
+				public void onErrorResponse(VolleyError arg0) {
+					// TODO Auto-generated method stub
+
+				}
+
+				@Override
+				public void onResponse(ImageContainer container, boolean b) {
+					zoomImageView.setImageBitmap(container.getBitmap());
+				}
+			});
 			reload_tv.setOnClickListener(new OnClickListener() {
 
 				@Override
 				public void onClick(View arg0) {
-					myImageLoader.displayImage(mPictures[position]);
+					// TODO
 				}
 			});
-			myImageLoader.mImageView = zoomImageView;
-			myImageLoader.mProgress_img = progress_img;
-			myImageLoader.mReload_tv = reload_tv;
-			myImageLoader.displayImage(mPictures[position]);
 			container.addView(view, LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
 			return view;
 		}
